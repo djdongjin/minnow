4c85ecf056a73b7dbdf22c0761abc560e35db8a5
Jin Dong <jin.dong@databricks.com>
2024-03-20 05:01:25 +0000

    pj0 - 4: byte stream
    Signed-off-by: Jin Dong <jin.dong@databricks.com>

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,65 +1,80 @@
 #include "byte_stream.hh"
+#include <cstdint>
 
 using namespace std;
 
-ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
+ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ), buffer_( queue<string>() ) {}
 
 bool Writer::is_closed() const
 {
-  // Your code here.
-  return {};
+  return closed_;
 }
 
 void Writer::push( string data )
 {
-  // Your code here.
-  (void)data;
-  return;
+  if ( closed_ || data.size() == 0 || capacity_ == 0 )
+    return;
+  if ( data.size() > capacity_ ) {
+    data = data.substr( 0, capacity_ );
+  }
+
+  buffer_.push( data );
+  bytes_pushed_ += data.size();
+  capacity_ -= data.size();
 }
 
 void Writer::close()
 {
-  // Your code here.
+  closed_ = true;
 }
 
 uint64_t Writer::available_capacity() const
 {
-  // Your code here.
-  return {};
+  return capacity_;
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  // Your code here.
-  return {};
+  return bytes_pushed_;
 }
 
 bool Reader::is_finished() const
 {
-  // Your code here.
-  return {};
+  return closed_ && buffer_.empty();
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  // Your code here.
-  return {};
+  return bytes_popped_;
 }
 
 string_view Reader::peek() const
 {
-  // Your code here.
-  return {};
+  if ( bytes_buffered() != 0 )
+    return buffer_.front();
+  else
+    return string_view();
 }
 
 void Reader::pop( uint64_t len )
 {
-  // Your code here.
-  (void)len;
+  while ( bytes_buffered() > 0 && len > 0 ) {
+    string& front = buffer_.front();
+    uint64_t bytes_to_pop;
+    if ( front.size() <= len ) {
+      bytes_to_pop = front.size();
+      buffer_.pop();
+    } else {
+      bytes_to_pop = len;
+      front.erase( 0, len );
+    }
+    bytes_popped_ += bytes_to_pop;
+    capacity_ += bytes_to_pop;
+    len -= bytes_to_pop;
+  }
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  // Your code here.
-  return {};
+  return bytes_pushed_ - bytes_popped_;
 }
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <queue>
 #include <string>
 #include <string_view>
 
@@ -25,6 +26,11 @@ protected:
   // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
   uint64_t capacity_;
   bool error_ {};
+
+  std::queue<std::string> buffer_;
+  bool closed_ {};
+  uint64_t bytes_pushed_ {};
+  uint64_t bytes_popped_ {};
 };
 
 class Writer : public ByteStream
